68d74173cfd27bcdb90375f9c3d746668bb4ac59
Sebastian Kraus <sekdiy@gmail.com>
2015-07-15 14:22:58 +0200

    improved the simulator example

--- examples/Simulator/Simulator.ino
+++ examples/Simulator/Simulator.ino
@@ -1,56 +1,43 @@
-#include <Streaming.h>  // http://arduiniana.org/libraries/streaming/
-#include <Relay.h>      // https://github.com/sekdiy/Relay
 #include <FlowMeter.h>  // https://github.com/sekdiy/FlowMeter
 
-// simulation period and pulse rate
-const unsigned long period = 1000;    // ms
-const unsigned long frequency = 10;   // Hz
+// let's provide our own sensor properties, including calibration points for error correction
+FlowSensorProperties MySensor = {60.0f, 4.5f, {1.2, 1.1, 1.05, 1, 1, 1, 1, 0.95, 0.9, 0.8}};
 
-// create a flow meter and a simulator (see notes on your Arduino model for pin numbers)
-FlowMeter Meter = FlowMeter(2);
-Relay Simulator = Relay(13);
-
-// define an 'interrupt service handler' (ISR) for every interrupt pin you use
-void MeterISR() {
-  // let our flow meter count the pulses
-  Meter.count();
-}
+// let's pretend there's a flow sensor connected to pin 3
+FlowMeter Meter = FlowMeter(3, MySensor);
 
 void setup() {
   // prepare serial communication
   Serial.begin(9600);
-
-  // enable a call to the 'interrupt service handler' (ISR) on every rising edge at the interrupt pin
-  // do this setup step for every ISR you have defined, depending on how many interrupts you use
-  attachInterrupt(INT0, MeterISR, RISING);
 
   // sometimes initializing the gear generates some pulses that we should ignore
   Meter.reset();
 }
 
 void loop() {
+  // randomly change simulation period and pulse rate
+  long frequency = random(MySensor.capacity * MySensor.kFactor);  // Hz
+  long period = random(3 * frequency, 5000);                      // ms
 
- // simulate flow meter pulses inbetween output updates
-  for (unsigned long int i = 0; i < period; i++) {
-    if (0 == i % (int) ((1000.0f * 1/frequency) / 2.0f)) {
-      Simulator.toggle();
-    } else {
-      delay(1);
-    }
+  // simulate random flow meter pulses within a random period
+  for (long i = 0; i < (long) ((float) period * (float) frequency / 1000.0f); i++) {
+      Meter.count();
   }
+
+  // wait that random period
+  delay(period);
 
   // process the counted ticks
   Meter.tick(period);
 
   // output some measurement result
-  Serial << "FlowMeter "
-         << "- flow rate: " << Meter.getCurrentFlowrate() << " l/min, "
-         << "error correction: " << Meter.getCurrentError() << " %, "
-         << "total volume: " << Meter.getTotalVolume() << " l, "
-         << "average flow rate: " << Meter.getTotalFlowrate() << " l/min, "
-         << "average error correction: " << Meter.getTotalError() << " %, "
-         << "current duration: " << Meter.getCurrentDuration() << " ms, "
-         << "total duration: " << Meter.getTotalDuration() / period << " s."
-         << endl;
-
- }
+  Serial.println("FlowMeter - period: " + String(Meter.getCurrentDuration() / 1000.0f, 3) + "s, " +
+                 "frequency: " + String(Meter.getCurrentFrequency(), 0) + "Hz, " +
+                 "volume: " + Meter.getCurrentVolume() + "l, " +
+                 "flow rate: " + Meter.getCurrentFlowrate() + "l/min, " +
+                 "error: " + Meter.getCurrentError() + "%, " +
+                 "total duration: " + Meter.getTotalDuration() / 1000.0f + "s, " +
+                 "total volume: " + Meter.getTotalVolume() + "l, " +
+                 "average flow rate: " + Meter.getTotalFlowrate() + "l/min, " +
+                 "average error: " + Meter.getTotalError() + "%.");
+}
