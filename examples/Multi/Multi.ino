08a39a909f1012db00f00e2a05f55642383a9891
Sebastian Kraus <sekdiy@gmail.com>
2018-10-17 02:00:16 +0200

    added multi sensor example (thanks to @highergroundstudio)

--- examples/Multi/Multi.ino
+++ examples/Multi/Multi.ino
@@ -1,15 +1,22 @@
 #include <FlowMeter.h>  // https://github.com/sekdiy/FlowMeter
 
 // connect a flow meter to an interrupt pin (see notes on your Arduino model for pin numbers)
-FlowMeter Meter = FlowMeter(2);
+FlowMeter Meter1 = FlowMeter(2);
+FlowMeter Meter2 = FlowMeter(3);
 
 // set the measurement update period to 1s (1000 ms)
 const unsigned long period = 1000;
 
 // define an 'interrupt service handler' (ISR) for every interrupt pin you use
-void MeterISR() {
+void Meter1ISR() {
     // let our flow meter count the pulses
-    Meter.count();
+    Meter1.count();
+}
+
+// define an 'interrupt service handler' (ISR) for every interrupt pin you use
+void Meter2ISR() {
+    // let our flow meter count the pulses
+    Meter2.count();
 }
 
 void setup() {
@@ -18,10 +25,12 @@ void setup() {
 
     // enable a call to the 'interrupt service handler' (ISR) on every rising edge at the interrupt pin
     // do this setup step for every ISR you have defined, depending on how many interrupts you use
-    attachInterrupt(INT0, MeterISR, RISING);
+    attachInterrupt(INT0, Meter1ISR, RISING);
+    attachInterrupt(INT1, Meter2ISR, RISING);
 
     // sometimes initializing the gear generates some pulses that we should ignore
-    Meter.reset();
+    Meter1.reset();
+    Meter2.reset();
 }
 
 void loop() {
@@ -29,10 +38,12 @@ void loop() {
     delay(period);
 
     // process the (possibly) counted ticks
-    Meter.tick(period);
+    Meter1.tick(period);
+    Meter2.tick(period);
 
     // output some measurement result
-    Serial.println("Currently " + String(Meter.getCurrentFlowrate()) + " l/min, " + String(Meter.getTotalVolume())+ " l total.");
+    Serial.println("Meter 1 currently " + String(Meter1.getCurrentFlowrate()) + " l/min, " + String(Meter1.getTotalVolume())+ " l total.");
+    Serial.println("Meter 2 currently " + String(Meter2.getCurrentFlowrate()) + " l/min, " + String(Meter2.getTotalVolume())+ " l total.");
 
     //
     // any other code can go here
--- examples/Simulator/Simulator.ino
+++ examples/Simulator/Simulator.ino
@@ -7,37 +7,37 @@ FlowSensorProperties MySensor = {60.0f, 4.5f, {1.2, 1.1, 1.05, 1, 1, 1, 1, 0.95,
 FlowMeter Meter = FlowMeter(3, MySensor);
 
 void setup() {
-  // prepare serial communication
-  Serial.begin(9600);
+    // prepare serial communication
+    Serial.begin(9600);
 
-  // sometimes initializing the gear generates some pulses that we should ignore
-  Meter.reset();
+    // sometimes initializing the gear generates some pulses that we should ignore
+    Meter.reset();
 }
 
 void loop() {
-  // randomly change simulation period and pulse rate
-  long frequency = random(MySensor.capacity * MySensor.kFactor);  // Hz
-  long period = random(3 * frequency, 5000);                      // ms
+    // randomly change simulation period and pulse rate
+    long frequency = random(MySensor.capacity * MySensor.kFactor);  // Hz
+    long period = random(3 * frequency, 5000);                      // ms
 
-  // simulate random flow meter pulses within a random period
-  for (long i = 0; i < (long) ((float) period * (float) frequency / 1000.0f); i++) {
-      Meter.count();
-  }
+    // simulate random flow meter pulses within a random period
+    for (long i = 0; i < (long) ((float) period * (float) frequency / 1000.0f); i++) {
+        Meter.count();
+    }
 
-  // wait that random period
-  delay(period);
+    // wait that random period
+    delay(period);
 
-  // process the counted ticks
-  Meter.tick(period);
+    // process the counted ticks
+    Meter.tick(period);
 
-  // output some measurement result
-  Serial.println("FlowMeter - period: " + String(Meter.getCurrentDuration() / 1000.0f, 3) + "s, " +
-                 "frequency: " + String(Meter.getCurrentFrequency(), 0) + "Hz, " +
-                 "volume: " + Meter.getCurrentVolume() + "l, " +
-                 "flow rate: " + Meter.getCurrentFlowrate() + "l/min, " +
-                 "error: " + Meter.getCurrentError() + "%, " +
-                 "total duration: " + Meter.getTotalDuration() / 1000.0f + "s, " +
-                 "total volume: " + Meter.getTotalVolume() + "l, " +
-                 "average flow rate: " + Meter.getTotalFlowrate() + "l/min, " +
-                 "average error: " + Meter.getTotalError() + "%.");
+    // output some measurement result
+    Serial.println("FlowMeter - period: " + String(Meter.getCurrentDuration() / 1000.0f, 3) + "s, " +
+                  "frequency: " + String(Meter.getCurrentFrequency(), 0) + "Hz, " +
+                  "volume: " + Meter.getCurrentVolume() + "l, " +
+                  "flow rate: " + Meter.getCurrentFlowrate() + "l/min, " +
+                  "error: " + Meter.getCurrentError() + "%, " +
+                  "total duration: " + Meter.getTotalDuration() / 1000.0f + "s, " +
+                  "total volume: " + Meter.getTotalVolume() + "l, " +
+                  "average flow rate: " + Meter.getTotalFlowrate() + "l/min, " +
+                  "average error: " + Meter.getTotalError() + "%.");
 }
